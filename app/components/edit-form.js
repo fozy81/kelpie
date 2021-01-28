c1be42eef444d52ff0d555871cfcbb286eee0a80
fozy81 <fozy81@hotmail.com>
2021-01-28 12:38:59 +0000

    fix: correct template selection picking

--- app/components/edit-form.js
+++ app/components/edit-form.js
@@ -13,7 +13,7 @@ export default class EditFormComponent extends Component {
     let formTemplateId = this.args.formTemplate.id
     this.store.findRecord('form-template', formTemplateId).then(function (formTemplate) {
       formTemplate.save();
-    })   
+    })
 
     // stop showing edit menu
     this.args.edit()
@@ -25,10 +25,9 @@ export default class EditFormComponent extends Component {
   @tracked selectedOptions = [];
   @action
   formChange(event) {
-
-    let selection = { id: event.target.id, value: event.target.value }  
-    console.log('selection:' + selection) 
-    if (this.selectedOptions.length === 0 ) {
+    let selection = { id: event.target.id, value: event.target.value }
+    console.log('selection:' + selection)
+    if (this.selectedOptions.length === 0) {
       this.selectedOptions.push(selection)
     } else {
       // eslint-disable-next-line no-inner-declarations
@@ -41,7 +40,6 @@ export default class EditFormComponent extends Component {
             found += true
           }
         }
-
         if (found === false) {
           selectedOptions = selectedOptions.push(selection)
         }
@@ -50,16 +48,12 @@ export default class EditFormComponent extends Component {
       let selectedOptions = this.selectedOptions
       let options = changeDesc(event.target.value, event.target.id, selectedOptions);
       this.selectedOptions = selectedOptions
-
     }
-          console.log(this.selectedOptions[0].value)
-        
-        
   }
 
   @service router;
   @action
-  saveReplaceForm(event) { 
+  saveReplaceForm(event) {
     console.log('saveReplaceForm: ')
     // event.preventDefault()
     let store = this.store
@@ -70,90 +64,99 @@ export default class EditFormComponent extends Component {
     let formTemplateId = this.args.formTemplate.id
     store.findRecord('form-template', formTemplateId, { include: 'questionTemplates' }).then(
       function (formTemplate) {
-      // convert checkbox string to boolean      
-      if(formTemplate.multiEntry === 'true' | formTemplate.multiEntry === true) {
-        formTemplate.multiEntry = true
-      } else {
-        formTemplate.multiEntry = false
-      }
-      console.log('multientry: ' + formTemplate.multiEntry)
-      formTemplate.save().then(function(formTemplates) {
+        // convert checkbox string to boolean      
+        if (formTemplate.multiEntry === 'true' | formTemplate.multiEntry === true) {
+          formTemplate.multiEntry = true
+        } else {
+          formTemplate.multiEntry = false
+        }
+        console.log('multientry check: ' + formTemplate.multiEntry)
+        formTemplate.save().then(function (formTemplates) {
 
-      // For question type 'dropdown' select options:
-     let questionTemplates = formTemplates.questionTemplates;
-        questionTemplates.map(function(question, index) {   
-          console.log('question map')    
-        store.findRecord('question-template', question.id).then( 
-        function(questionTemplate) {
-         // console.log('selectedOptions: ' + selectedOptions[index].value )
-          if(selectedOptions.length === 0) {
-            questionTemplate.type = "text"      
-          } else {
-            questionTemplate.type = selectedOptions[index].value   
-          }
-          questionTemplate.response = ''                 
-          questionTemplate.save()
-        })
-       
-     })
-    }) 
-    }).then(function() {
-      let formId  = args.forms.id
-    
-      let form = store.peekRecord('form', formId);
-      form.destroyRecord();
-      // add new form
-      const taskId = router.currentRoute.params.task_id
-      console.log(taskId)
-      let myTask = store.peekRecord('task', taskId);      
-      let formTemplate = store.peekRecord('form-template', formTemplateId);
-      let formRecord = store.createRecord('form', {
-        title: formTemplate.title,
-        description: formTemplate.description,
-        rep: 1,
-        edit: false, 
-        multiEntry: formTemplate.multiEntry,    
-        dateCreated: new Date(),
-        templateId: formTemplate.id,
-        task: myTask,
-        display: false
-      }
-      )
-      formRecord.save().then(function(form) {
-        console.log(form.templateId)
-        let questionTemplates = formTemplate.questionTemplates      
-        let myForm = store.peekRecord('form', form.id);
-        questionTemplates.map(function(questionTemplate) {
-          console.log('question: ' + questionTemplate.question)
-          console.log('multi-entry? : ' + myForm.multiEntry) 
-          let response = ''
-             if(questionTemplate.default) {
-              response = questionTemplate.default
-             }            
-          let question = store.createRecord('question', {
-            question: questionTemplate.question,
-            response: response,
-            rep: 1,
-            multiEntry: myForm.multiEntry,
-            type: questionTemplate.type,
-            pos: questionTemplate.pos,
-            options: questionTemplate.options,
-            required: questionTemplate.required,
-            min: questionTemplate.min,
-            max: questionTemplate.max,
-            step: questionTemplate.step,
-            default: questionTemplate.default,
-            form: myForm
+          // For question type 'dropdown' select options:
+          let questionTemplates = formTemplates.questionTemplates;
+          questionTemplates.map(function (question, index) {
+           
+            store.findRecord('question-template', question.id).then(
+              function (questionTemplate) {
+                // console.log('selectedOptions: ' + selectedOptions[index].value )
+                console.log('question map')
+                if (selectedOptions.length === 0) {
+                  questionTemplate.type = "text"
+                } else {
+                   console.log('question map')
+                  selectedOptions.map(function(selected){
+                    console.log('selected id: ' + selected.id )
+                    console.log('questionTemplate id: ' + questionTemplate.id )
+                    if(selected.id == questionTemplate.id) {
+                      questionTemplate.type = selected.value
+                    }
+                  })
+                  
+                }
+                questionTemplate.response = ''
+                questionTemplate.save()
+              })
+
           })
-          question.save()
+        })
+      }).then(function () {
+        let formId = args.forms.id
+
+        let form = store.peekRecord('form', formId);
+        form.destroyRecord();
+        // add new form
+        const taskId = router.currentRoute.params.task_id
+        console.log(taskId)
+        let myTask = store.peekRecord('task', taskId);
+        let formTemplate = store.peekRecord('form-template', formTemplateId);
+        let formRecord = store.createRecord('form', {
+          title: formTemplate.title,
+          description: formTemplate.description,
+          rep: 1,
+          edit: false,
+          multiEntry: formTemplate.multiEntry,
+          dateCreated: new Date(),
+          templateId: formTemplate.id,
+          task: myTask,
+          display: false
+        }
+        )
+        formRecord.save().then(function (form) {
+          console.log(form.templateId)
+          let questionTemplates = formTemplate.questionTemplates
+          let myForm = store.peekRecord('form', form.id);
+          questionTemplates.map(function (questionTemplate) {
+            console.log('question: ' + questionTemplate.question)
+            console.log('multi-entry? : ' + myForm.multiEntry)
+            let response = ''
+            if (questionTemplate.default) {
+              response = questionTemplate.default
+            }
+            let question = store.createRecord('question', {
+              question: questionTemplate.question,
+              response: response,
+              rep: 1,
+              multiEntry: myForm.multiEntry,
+              type: questionTemplate.type,
+              pos: questionTemplate.pos,
+              options: questionTemplate.options,
+              required: questionTemplate.required,
+              min: questionTemplate.min,
+              max: questionTemplate.max,
+              step: questionTemplate.step,
+              default: questionTemplate.default,
+              form: myForm
+            })
+            question.save()
+          })
         })
       })
-    })   
-     
+
 
     // remove existing formlet questionTemplates = formTemplate.questionTemplates 
-    
-  
+
+
 
     // stop showing edit menu
     //this.args.edit()
@@ -165,17 +168,17 @@ export default class EditFormComponent extends Component {
   addQuestion() {
     let formTemplateId = this.args.formTemplate.id
     console.log(formTemplateId)
-    let formTemplate = this.store.peekRecord('form-template', formTemplateId)    
-    let questions = formTemplate.questionTemplates    
+    let formTemplate = this.store.peekRecord('form-template', formTemplateId)
+    let questions = formTemplate.questionTemplates
     let position = questions.length + 1
-    
+
     let newQuestion = this.store.createRecord('question-template', {
       multiEntry: false,
       pos: position,
-      formTemplate: formTemplate      
+      formTemplate: formTemplate
     })
 
-    
+
     newQuestion.save().then(console.log('question?' + newQuestion.question))
     this.showthis = true
   }
@@ -188,27 +191,27 @@ export default class EditFormComponent extends Component {
   @tracked showField = '';
   @action
   showInput(index) {
-        this.showField = index    
-        console.log(this.showField)
+    this.showField = index
+    console.log(this.showField)
   }
 
   @action
-  removeFormTemplate(id){
- 
-   console.log('remove question!' + id)
- 
-   let form = this.store.peekRecord('form-template', id);
-   console.log('remove question!' + form)
-   if(form.archive === true) {
-    form.archive = false
-   } else {
-   form.archive = true
-   }
-   form.save()
-   this.args.edit()
+  removeFormTemplate(id) {
+
+    console.log('remove question!' + id)
+
+    let form = this.store.peekRecord('form-template', id);
+    console.log('remove question!' + form)
+    if (form.archive === true) {
+      form.archive = false
+    } else {
+      form.archive = true
+    }
+    form.save()
+    this.args.edit()
   }
 
 
 
-  
+
 }
