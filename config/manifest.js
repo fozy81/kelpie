e458ea881d86387a5a88d1e0b6c5f45de07d8cc9
fozy81 <fozy81@hotmail.com>
2021-05-03 15:42:10 +0100

    feat: web app and service worker

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "kelpie",
+    short_name: "kelpie",
+    description: "",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/images/unicorn-1.png",
+        sizes: "256x256"       
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
